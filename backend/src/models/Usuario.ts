31d7dc2b692b85b8317b9e999dcce4cdeda47ed2
Nitza <nitza.vz@hotmail.com>
2025-09-17 23:46:27 -0700

    Modelado - parte 1
    Usuarios

---
+++ backend/src/models/Usuario.ts
@@ -0,0 +1,24 @@
+export interface IUsuario {
+    id: string
+    name: string
+    rol: string
+    password: string
+}
+
+export class Usuario implements IUsuario {
+    id: string
+    name: string
+    rol: string
+    password: string
+
+    constructor(id: string, name: string, rol: string, password: string) {
+        this.id = id
+        this.name = name
+        this.rol = rol
+        this.password = password
+    }
+
+    validate(): boolean {
+        return !!this.name && !!this.password
+    }
+}
